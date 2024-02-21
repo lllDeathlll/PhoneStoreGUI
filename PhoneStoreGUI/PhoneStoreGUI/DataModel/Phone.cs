82b7757d468a32c050b36e75a3eccb499c0d62e5
Roman Avdeev <romanaavdeev@gmail.com>
2024-02-21 23:02:15 +0400

    Fix Date loading

--- PhoneStoreGUI/PhoneStoreGUI/DataModel/Phone.cs
+++ PhoneStoreGUI/PhoneStoreGUI/DataModel/Phone.cs
@@ -4,24 +4,18 @@ namespace PhoneStoreGUI.DataModel;
 
 public class Phone
 {
-    private DateOnly _releaseDate;
+    private DateTimeOffset _releaseDate;
 
     public string Model { get; init; }
     public int Price { get; init; }
-    public DateOnly ReleaseDate
+    public DateTimeOffset ReleaseDate
     {
         get => _releaseDate;
         init
         {
-            var now = DateOnly.FromDateTime(DateTime.Now);
-            if (value > now)
-            {
-                _releaseDate = now;
-            }
-            else
-            {
-                _releaseDate = value;
-            }
+            //var now = DateOnly.FromDateTime(DateTime.Now);
+            var now = DateTime.Now;
+            _releaseDate = value > now ? now : value;
         }
     }
 
@@ -29,6 +23,11 @@ public class Phone
     {
         Model = model;
         Price = price;
-        ReleaseDate = DateOnly.FromDateTime(releaseDate.DateTime);
+        ReleaseDate = releaseDate;
+    }
+
+    public DateOnly GetReleaseDate
+    {
+        get => DateOnly.FromDateTime(ReleaseDate.DateTime);
     }
 }
